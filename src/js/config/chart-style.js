d903df4bf244d63d17d0b1ac52b698083c2ea47e
mhkeller <michael.keller@gmail.com>
2015-07-07 16:13:06 -0400

    basic color change

--- src/js/config/chart-style.js
+++ src/js/config/chart-style.js
@@ -18,6 +18,7 @@ var chart_style = {
 	overtick_top: "0.8em",
 	overtick_bottom: 10,
 	numColors: 11,
+	// numColors: 11,
 	xOverTick: "1em", // horizontal the distance between the yAxes and xAxis
 	creditMargin: "0.6em"
 };
